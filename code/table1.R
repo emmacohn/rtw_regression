f774ad2bd638e2722efbd0ef45c1d3833af6fc14
Emma Cohn <ecohn@epi.org>
2026-07-02 11:37:19 -0400

    edit

--- code/table1.R
+++ code/table1.R
@@ -118,6 +118,45 @@ demos_table25 <- map(.x = c("wbhao", "educ"), .f = ~ demos_c_fun(.x)) |>
   bind_rows(demos25) |> 
   rename(rtw = 2, non_rtw = 3)
 
+###### Switchers for 2023-2025 ######
+demos_d <- df |> filter(year %in% c(2023:2025), metstat == 0 | metstat == 1, statefips %in% c("IN", "WI", "WV", "KY"))
+
+demos_d_fun <- function(x) {
+  demos_d |>
+    mutate(!!x := to_factor(.data[[x]])) |>
+    summarise(pop = sum(wgt/3),
+              n = n(),
+              .by = all_of(c(x, "rtw_status"))
+             ) |>
+    mutate(share = pop/sum(pop),
+           .by = "rtw_status") |>
+    pivot_wider(id_cols = all_of(x), names_from = rtw_status, values_from = share)
+}
+
+# calculate demos results for non-share statistics
+demos_switch <- demos_d |> 
+  summarize(age = weighted.mean(age, w = (wgt/3)),
+            sex = 1-(weighted.mean(female, w= (wgt/3))), ## share male
+            metstat = weighted.mean(metstat, w = (wgt/3)),
+            paidhre = weighted.mean(paidhre, w = (wgt/3)),
+            ft = weighted.mean(ft, w = (wgt/3)),
+            union = weighted.mean(union, w = (wgt/3)),
+            avg_wage = weighted.mean(realwage25, w = (wgt/3)),
+            med_wage = weighted_median(realwage25, w = (wgt/3)),
+            urate = weighted.mean(urate, w = (wgt/3)),
+            rpp = weighted.mean(rpp, w = (wgt/3)),
+            n=n(),
+           .by = rtw_status) |> 
+  pivot_longer(cols = -rtw_status, names_to = "demographic") |>
+  pivot_wider(names_from = rtw_status, values_from = value)
+
+# use demos_fun to calculate share by value of remaining variables
+demos_table3 <- map(.x = c("wbhao", "educ"), .f = ~ demos_d_fun(.x)) |>
+  reduce(bind_rows) |>
+  unite(col = "demographic", c(wbhao, educ), na.rm = TRUE) |> 
+  bind_rows(demos_switch) |> 
+  rename(switch = 2)
+
 
 wb$
   # add new worksheet
@@ -128,4 +167,6 @@ wb$
   add_data(x = "2024", start_col = 5)$
   add_data(x = demos_table24, start_col = 5, start_row = 2)$
   add_data(x = "2025", start_col = 9)$
-  add_data(x = demos_table25, start_col = 9, start_row = 2)
+  add_data(x = demos_table25, start_col = 9, start_row = 2)$
+  add_data(x = "switchers", start_col = 13)$
+  add_data(x = demos_table3, start_col = 9, start_row = 2)
